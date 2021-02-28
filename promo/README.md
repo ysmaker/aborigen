7e97d61a1317033bb9a9608543d344d37c702d80
George Kozaev <george.kozaev@gmail.com>
2021-03-01 02:45:51 +0300

    webpack

--- promo/README.md
+++ promo/README.md
@@ -1,20 +1,16 @@
-# aborigen<br/><br/>
-
-
-
-## Webpack:<br/>
+# Готовая сборка webpack
 Выполнить установку - npm i
 
 ## Команды:<br/>
 
 npm run dev - запуск сборки в dev режиме<br/>
 npm run build - запуск сборки в production режиме<br/>
-npm run start - запуск сервера в dev режиме<br/>
+npm run watch - запуск сервера в dev режиме<br/>
 
 ## Директория src содержит исходники:<br/>
 
 -fonts - подключаемые шрифты <br/>
--img - изображения <br/>
--scripts - js-скрипты <br/>
--styles - scss стили <br/>
--template - twig шаблоны <br/>
+-images - изображения <br/>
+-scripts - js -скрипты <br/>
+-styles - sass стили <br/>
+-template - pug шаблоны <br/>
---
+++ promo/webpack.config.js
@@ -0,0 +1,81 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+					loader: MiniCssExtractPlugin.loader,
+					options: {
+						sourceMap: true
+					}
+				},
+				{
+					loader: "css-loader",
+					options: {
+						sourceMap: true
+					}
+				},
+				{
+					loader: "sass-loader",
+					options: {
+						sourceMap: true
+					}
+				}]
+			},
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]'
+						}
+					}
+				]
+			},
+			{
+				test: /\.twig$/,
+				use: {
+					loader: 'twig-loader',
+					options: {
+					},
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery/dist/jquery.min.js",
+			jQuery: "jquery/dist/jquery.min.js",
+			"window.jQuery": "jquery/dist/jquery.min.js"
+		}),
+		new HtmlWebPackPlugin({
+			template: "./src/template/pages/index.twig",
+			filename: "./index.html"
+		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		new CopyWebpackPlugin([
+			{ from: './src/img', to: './img' },
+			{ from: './src/fonts', to: './fonts' },
+		])
+	]
+};
