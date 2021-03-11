5ca7d9ca71df1e5d3787c0b0ada7352c5c96939e
George Kozaev <george.kozaev@gmail.com>
2021-03-11 08:56:07 +0300

    reg

---
+++ registration/src/index.js
@@ -0,0 +1,6 @@
+import html from "./template/pages/index.twig";
+// Import styles
+import style from "./styles/main.scss";
+// Import scripts
+import script from "./scripts/main.js";
+// console.log('complete');
