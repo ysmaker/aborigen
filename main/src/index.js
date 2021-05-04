1d2a940dbbc3727eddd9780ab2bf43576dab4295
George Kozaev <george.kozaev@gmail.com>
2021-05-04 12:25:32 +0300

    faq and pop

--- main/src/index.js
+++ main/src/index.js
@@ -1,11 +1,12 @@
 import html from "./template/pages/index.twig";
 // Import styles
 import style from "./styles/main.scss";
-// Import scripts
 
 //Import vendors
+import slick from "./scripts/vendors/slick.min.js";
 import datepicker from "./scripts/vendors/datepicker.min.js";
 
-
+// Import scripts
 import script from "./scripts/main.js";
+
 // console.log('complete');
