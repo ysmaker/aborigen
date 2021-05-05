0887ccc7816255aa9c6c729cbd90e5ea7305115b
George Kozaev <george.kozaev@gmail.com>
2021-05-05 09:32:18 +0300

    slider

--- main/src/index.js
+++ main/src/index.js
@@ -5,6 +5,7 @@ import style from "./styles/main.scss";
 //Import vendors
 import slick from "./scripts/vendors/slick.min.js";
 import datepicker from "./scripts/vendors/datepicker.min.js";
+import inputmask from "./scripts/vendors/inputmask.min.js";
 
 // Import scripts
 import script from "./scripts/main.js";
